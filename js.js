2b27898766e9d88b5cb54e3934b1d1c34810605d
marmarx <78326372+marmarx@users.noreply.github.com>
2025-03-05 19:59:07 -0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,21 @@
+function factor(n){
+  if (n < 2) return [n]
+
+  let fatores = [];
+  let divisor = 2;
+
+  while (n > 1) {
+    while (n % divisor === 0){
+      fatores.push(divisor);
+      n /= divisor;
+    }
+    divisor++;
+    if(divisor * divisor > n && n > 1) {
+      fatores.push(n);
+      break;
+    }
+  }
+  return fatores;
+}
+
+const func = val => document.getElementById("demo").innerHTML = fatores(val)
